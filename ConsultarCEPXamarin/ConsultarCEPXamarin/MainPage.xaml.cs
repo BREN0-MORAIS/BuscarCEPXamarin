eb034a8b04252e9b9e76b1af2437bafef5539d9a
BRENO MORAIS <62041808+BREN0-MORAIS@users.noreply.github.com>
2021-01-01 20:11:33 -0400

    init

---
+++ ConsultarCEPXamarin/ConsultarCEPXamarin/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using ConsultarCEPXamarin.Servico;
+using ConsultarCEPXamarin.Servico.Modelo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ConsultarCEPXamarin
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            btnBuscarCep.Clicked += BuscarCep;
+        }
+
+        private void BuscarCep(object sender, EventArgs args)
+        {
+            //logica do problema
+            //Validação
+            //Buscar Na Url
+          string cep = etCep.Text.Trim(); //trim remove todos os espaço que o usuario digito
+          Endereco end = ViaCepServicos.BuscarEnderecoViaCep(cep);
+
+            resultado.Text =string.Format("Edereco: {0},{1},{2}",end.localidade,end.bairro,end.logradouro) ;
+        }
+    }
+}
---
+++ ConsultarCEPXamarin/ConsultarCEPXamarin/Servico/ViaCepServicos.cs
@@ -0,0 +1,28 @@
+﻿using ConsultarCEPXamarin.Servico.Modelo;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace ConsultarCEPXamarin.Servico
+{
+    public class ViaCepServicos
+    {
+        private static string EnderecoUrl = @"https://viacep.com.br/ws/{0}/json/";
+
+        public  static Endereco BuscarEnderecoViaCep(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoUrl, cep);
+
+            //vai fazer a busca na url
+            WebClient wc = new WebClient();
+           string conteudo =    wc.DownloadString(NovoEnderecoURL);
+
+            //converter para um objeto do Tipo Endereço
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(conteudo);
+
+            return end;
+        }
+    }
+}
